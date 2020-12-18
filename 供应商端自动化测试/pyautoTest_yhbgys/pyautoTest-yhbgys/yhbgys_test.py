e849e0a3b4fbfde076bcf5ab33947f08b3f86015
陈泓泽 <1700191526@qq.com>
2020-12-18 10:10:27 +0800

    提交测试代码

--- 供应商端自动化测试/pyautoTest_yhbgys/pyautoTest-yhbgys/yhbgys_test.py
+++ 供应商端自动化测试/pyautoTest_yhbgys/pyautoTest-yhbgys/yhbgys_test.py
@@ -3,23 +3,28 @@ import sys
 sys.path.append("test_dir")
 
 if __name__ == '__main__':
-    choose = input("请输入测试项目：0.测试全部。1.测试登录。2.测试主页。3.测试订单。4.测试耗材。5.测试配送。6.测试退货。7.测试结算。")
-    if choose == "0":
-        pytest.main()
-    elif choose == "1":
-        pytest.main(["test_dir/test_login.py"])
-    elif choose == "2":
-        pytest.main(["test_dir/test_index.py"])
-    elif choose == "3":
-        pytest.main(["test_dir/test_order.py"])
-    elif choose == "4":
-        pytest.main(["test_dir/test_consumable.py"])
-    elif choose == "5":
-        pytest.main(["test_dir/test_agent.py"])
-    elif choose == "6":
-        pytest.main(["test_dir/test_return.py"])
-    elif choose == "7":
-        pytest.main(["test_dir/test_close.py"])
-    else:
+    test_file_list = ["test_dir/test_login.py",
+                      "test_dir/test_index.py",
+                      "test_dir/test_order.py",
+                      "test_dir/test_consumable.py",
+                      "test_dir/test_agent.py",
+                      "test_dir/test_return.py",
+                      "test_dir/test_close.py",
+                      "test_dir/test_setting.py"]
+    choose = input("请输入测试项目：0.测试全部。1.测试登录。2.测试主页。3.测试订单。4.测试耗材。5.测试配送。6.测试退货。7.测试结算。8.测试设置。")
+    try:
+        choose = int(choose)-1
+        if choose == -1:
+            # print(choose)
+            pytest.main(test_file_list)
+            pass
+        else:
+            print(test_file_list[choose])
+            pytest.main([test_file_list[choose]])
+            pass
+        pass
+    except Exception as e:
+        print(e)
         print("输入错误")
+        pass
     pass
